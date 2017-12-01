462b2df218b6571b85dc85493a4189cc7132df47
Tanya <taningreen@gmail.com>
2017-12-01 11:41:22 +0000

    step5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128180253) do
+ActiveRecord::Schema.define(version: 20171130221614) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
